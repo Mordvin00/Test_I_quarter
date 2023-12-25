ad10fc1f0d588c25224ffa8b270330d2d881a8db
Mordvin <mordvin00@gmail.com>
2023-12-25 13:39:28 +0300

    add ModArrayOfStrings - Создал программу модификации массива строк по количеству символов <=3

---
+++ ModArrayOfStrings/Program.cs
@@ -0,0 +1,58 @@
+﻿/* Задача:
+Написать программу, которая из имеющегося массива строк
+формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → [] */
+
+
+string[] GetArrayStringConsole (string inConsolSimvol)
+{
+    string[] arraySimvol = new string[inConsolSimvol.Length];
+    arraySimvol = inConsolSimvol.Split(" ");
+    return arraySimvol;
+}
+string[] GetArrayThreeSimvol (string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        string simvol = array[i];
+        if (simvol.Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] arrayThreeSimvol = new string[count];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        string simvol = array[i];
+        if (simvol.Length <= 3)
+        {
+            arrayThreeSimvol[j] = simvol;
+            j++;
+        }
+    }
+    return arrayThreeSimvol;
+}
+void Print (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"“{array[i]}” ");
+    }
+    Console.WriteLine();
+}
+Console.Write("Введите набор строк латинией через пробел: ");
+string inStringSimvol = (Console.ReadLine()!);
+string[] arraySimvol = GetArrayStringConsole(inStringSimvol);
+Console.WriteLine();
+Console.WriteLine("Введённые строки:");
+Print(arraySimvol);
+Console.WriteLine();
+Console.WriteLine("Введённые строки длиной меньшей либо равной 3 символам:");
+Print(GetArrayThreeSimvol(arraySimvol));
+Console.WriteLine();
